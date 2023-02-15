a778c3665f0cacbd2431ed2407bf3b1c3c129b01
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-15 09:39:42 -0700

    Logged students score

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,15 @@
 class Student
   attr_reader :name,
-              :age
+              :age,
+              :scores
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @age = attributes.fetch(:age)
+    @scores = []
+  end
+
+  def log_score(score)
+    scores << score
   end
 end
+
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Student do
     expect(student.name).to eq("Morgan")
     expect(student.age).to eq(21)
   end
+
+  describe '#logs sudents scores' do
+    it 'will log the students score' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.scores).to eq([])
+      score_1 = student.log_score(89)
+      score_2 = student.log_score(78)
+      expect(student.scores).to eq([89, 78])
+    end
+
+    
+
+  end
 end
