993815bd893d585bcbb1300bfbac0c693045b3e5
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-15 10:10:24 -0700

    Added attributes

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Course do
     expect(course).to be_an_instance_of(Course)
   end
 
+  it' has attributes' do
+    course = Course.new("Calculus", 2)
+
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+  end
+
 
 
 
