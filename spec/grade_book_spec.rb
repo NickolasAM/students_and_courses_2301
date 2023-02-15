9b91b16d64ccebf62b250ec7404eaa0d80fea4f4
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-15 10:57:40 -0700

    Almost finished iteration 3

--- spec/grade_book_spec.rb
+++ spec/grade_book_spec.rb
@@ -13,7 +13,15 @@ RSpec.describe Gradebook do
     expect(gradebook).to be_an_instance_of(Gradebook)
   end
 
+  it 'has attributes' do
+    gradebook = Gradebook.new(instructor = 'Abdul')
+    course = Course.new("Calculus", 2)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29}) 
 
+
+
+  end
 
 
 
